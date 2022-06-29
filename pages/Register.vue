2d28d8fdf7e379809a48bd6027ebe0328457326a
Sergey-dev-source <chsergdevelopment@gmail.com>
2022-06-29 18:30:35 +0400

    test

---
+++ pages/Register.vue
@@ -0,0 +1,72 @@
+<template>
+   <v-app id="inspire">
+      <v-content>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form>
+                           <v-text-field
+                               name="name"
+                               v-model="name"
+                               label="Name"
+                               type="text"
+                           ></v-text-field>
+                           <v-text-field
+                               name="email"
+                               v-model="email"
+                               label="Login"
+                               type="text"
+                           ></v-text-field>
+                           <v-text-field
+                               id="password"
+                               name="password"
+                               v-model="password"
+                               label="Password"
+                               type="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" @click="reg">Login</v-btn>
+                     </v-card-actions>
+                  </v-card>
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-content>
+   </v-app>
+</template>
+
+<script>
+export default {
+   name: "Register",
+   data() {
+      return {
+         name: '',
+         email: '',
+         password: '',
+
+      }
+   },
+   methods: {
+      reg() {
+         console.log('ok')
+         this.$axios.$post('api/register',{
+               name: this.name,
+               email: this.email,
+               password: this.password
+         })
+      }
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
